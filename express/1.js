9cca297c1531096c4f10017d5c9d2e9a4def6eb3
zhengfh <438570993@qq.com>
2019-03-25 22:44:35 +0800

    add get post api

--- express/1.js
+++ express/1.js
@@ -22,17 +22,44 @@ httpServer.listen (9090, function () {
 });
 httpsServer.listen (8990, function () {
 	console.log ("https server is running 8990");
-	
 });
 
 //  主页输出 "Hello World"
 app.get('/', function (req, res) {
    console.log("主页 GET 请求");
    if (req.protocol == 'https') {
-	   res.status(404).send ("welcome to safety land!8990 https!");
+	   res.status(200).send ("https get req");
    } else {
-	   res.status(200).send ("welcome 89909089");
+	   res.status(200).send ("http get req");
    }
 });
+ 
+ //  POST 请求
+app.post('/', function (req, res) {
+   console.log("主页 POST 请求");
+   res.send('Hello POST');
+})
+
+//  /del_user 页面响应
+app.delete('/del_user', function (req, res) {
+   console.log("/del_user 响应 DELETE 请求");
+   res.send('删除页面');
+})
+
+//  /list_user 页面 GET 请求
+app.get('/list_user', function (req, res) {
+   console.log("/list_user GET 请求");
+   res.send('用户列表页面');
+})
+
+// 对页面 abcd, abxcd, ab123cd, 等响应 GET 请求
+app.get('/ab*cd', function(req, res) {   
+   console.log("/ab*cd GET 请求");
+   res.send('正则匹配');
+})
+ 
+ 
+ 
+ 
  
  
