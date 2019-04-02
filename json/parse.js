e93234d65e068c9ed4c6245604a1498dd67cd4f6
zhengfh <zhengfh@sinoregal.cn>
2019-04-02 18:36:10 +0800

    添加射速和火力配置

--- json/parse.js
+++ json/parse.js
@@ -1,7 +1,13 @@
 var CONFPATH = "./equipment.json";
+var CONFPATH1 = "./fire_rate.json";
+
 var fs = require('fs');
 var result = JSON.parse(fs.readFileSync(CONFPATH));
 for (var i in result) {
   console.log(result[i].id);
 }
 
+var result1 = JSON.parse(fs.readFileSync(CONFPATH1));
+for (var i in result1) {
+  console.log(result1[i].level);
+}
